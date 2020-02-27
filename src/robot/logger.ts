13d84d70b2e172a2394ada1a166b2a6263c40700
Kris Savoia <kris.savoia@gmail.com>
2020-02-27 13:50:02 +1100

    Refactor robot logger for ts

--- src/robot/logger.ts
+++ src/robot/logger.ts
@@ -1,11 +1,11 @@
 export const prefix = '[robot]'
 
-function logger(type, details=[]) {
+function logger(type: string, details: string[] = []) {
     if (messages[type]) return `${messages[type]} ${details.join(', ')}`
     return `${prefix} Something is wrong` 
 }
 
-const messages = {
+const messages: {[key: string]: string} = {
     notPlacedYet: `${prefix} Hey. I'm not on the board yet. Try the PLACE command`,
     potentiallyOffTable: `${prefix} Do you want me to fall onto the floor!?!?`,
     placeBearing: `${prefix} <Facing> should be one of`,
